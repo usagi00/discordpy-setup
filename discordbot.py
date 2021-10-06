1cfb4ef118b576e60d70f3729f6e5c303c8dcd13
usagi00 <92000974+usagi00@users.noreply.github.com>
2021-10-06 12:32:10 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -16,6 +16,19 @@ async def on_command_error(ctx, error):
 async def ping(ctx):
     await ctx.send('pong')
     
+
+data = [
+    ['2019-07-01 15:00:00','9997','740'],
+    ['2019-07-02 15:00:00','9997','749'],
+    ['2019-07-03 15:00:00','9997','757'],
+    ['2019-07-04 15:00:00','9997','769'],
+    ['2019-07-05 15:00:00','9997','762'],
+    ['2019-07-08 15:00:00','9997','760']
+]
+
+
+df = pd.DataFrame(data)
+print(df)
     
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
