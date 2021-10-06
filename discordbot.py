edbea9b3398da7fcea2f9eb6cd5a6d20b51a3fc6
usagi00 <92000974+usagi00@users.noreply.github.com>
2021-10-06 10:33:14 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -12,7 +12,11 @@ async def on_command_error(ctx, error):
     await ctx.send(error_msg)
 
 
-
+@bot.command()
+async def ping(ctx):
+    await ctx.send('pong')
+    
+    
 @bot.command()
 async def neko(ctx):
     await ctx.send('にゃーん')
