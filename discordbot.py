77074cdaf235b32bf27942c91fb2fbe600338146
usagi00 <92000974+usagi00@users.noreply.github.com>
2021-10-06 15:20:17 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -15,21 +15,7 @@ async def on_command_error(ctx, error):
 @bot.command()
 async def ping(ctx):
     await ctx.send('pong')
-   
 
-code,name,weapon,attribute,affiliation,birthday,basishp,basisdefence,basisattack,criticalhit,critdamage,specificbonus
-RI,雷電将軍,長柄,雷,稲妻城,6/26,1005→12907,61→789,26→337,5.0%,50.0%,0.0%→32.0%
-
-
-@bot.command()
-async def intro(ctx, *args):
-   namepr = namelist.get(args[0])
-   msg = introlist.get(namepr)
-   
-   embed=discord.Embed(color=0xff2e2e)
-   embed.add_field(name=args[0], value=msg, inline=False)
-   await ctx.send(embed=embed)
-    
 
 token = getenv('DISCORD_BOT_TOKEN')
 bot.run(token)
